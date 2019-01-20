e4221aea6dd3c1d045ac2bb5a178edbe36e1d071
Yasin Karaca <yasin.karaca@hetic.net>
2019-01-20 21:33:25 +0100

    add into db from input

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Students } from '../api/students.js';
 import Student from './Student.js';
+import ReactDOM from 'react-dom';
  
 
 class App extends Component {
-  getStudents() {
-    return [
-      { _id: 1, name: 'Yasin' },
-      { _id: 2, name: 'Clément' },
-      { _id: 3, name: 'Mael' },
-    ];
-  }
+   handleSubmit(event) {
+    event.preventDefault();
+
+    const name = ReactDOM.findDOMNode(this.refs.nameInput).value.trim();
+ 
+    Students.insert({
+      name,
+      createdAt: new Date(),
+    });
+ 
+    ReactDOM.findDOMNode(this.refs.nameInput).value = '';
+  } 
  
   renderStudents() {
     return this.props.students.map((student) => (
@@ -24,6 +30,15 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Student list</h1>
+
+          <form className="new-student" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="nameInput"
+              placeholder="Type to add new students"
+            />
+          </form>
+
         </header>
  
         <ul>
--- server/main.js
+++ server/main.js
@@ -2,5 +2,5 @@ import { Meteor } from 'meteor/meteor';
 import '../imports/api/students.js';
 
 Meteor.startup(() => {
-
+  //
 });
