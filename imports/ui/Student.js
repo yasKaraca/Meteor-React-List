940c3de67eeabde6bb08c93209ad59a7280ba094
Yasin Karaca <yasin.karaca@hetic.net>
2019-01-20 22:32:03 +0100

    enable to add and delete

--- imports/ui/Student.js
+++ imports/ui/Student.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
- 
+import { Students } from '../api/students.js';
+
 export default class Student extends Component {
+//   editStudent() {
+//     Students.update({name: document.getElementById('render-target').value});
+//   }
+ 
+  deleteThisStudent() {
+    Students.remove(this.props.student._id);
+  }
+
   render() {
+    const studentClassName = this.props.student.checked ? 'checked' : '';  
     return (
-      <li>{this.props.student.name}</li>
+        <li className={studentClassName}>
+        <button className="delete" onClick={this.deleteThisStudent.bind(this)}>
+          &times;
+        </button>
+
+        <span className="text">{this.props.student.name}</span>    
+      </li>
     );
   }
 }
